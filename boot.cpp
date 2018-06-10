55c85eaa07e8f4f06363af471623b437607415f8
Claudio Dettoni <cd611@cl.cam.ac.uk>
2018-06-10 14:53:09 +0200

    Implemented scroll

--- boot.cpp
+++ boot.cpp
@@ -2,6 +2,24 @@ asm("jmp boot_main\n");
 
 #include <stdint.h>
 
+uint8_t inb(uint16_t port){
+	uint8_t ret;
+    asm volatile ( 
+		"inb %1, %0;"
+        : "=a"(ret)
+        : "d"(port) 
+    );
+    return ret;
+}
+
+void outb(uint16_t port, uint8_t data) {
+    asm volatile (
+        "outb %1, %0;"
+        :
+        : "d" (port), "a" (data)
+    );
+}
+
 void memset(unsigned char* mem, unsigned char c, int n) {
     while(n--) {
         *mem = c;
@@ -9,8 +27,16 @@ void memset(unsigned char* mem, unsigned char c, int n) {
     }
 }
 
+void memcpy(unsigned char* dst, unsigned char* src, int n) {
+    while(n--) {
+        *dst = *src;
+        dst++;
+        src++;
+    }
+}
+
 void reverse_inplace(unsigned char* mem, int n) {
-    unsigned char* end = mem + n;
+    unsigned char* end = mem + n - 1;
     unsigned char* start = mem;
     while(end > start) {
         char tmp = *end;
@@ -61,12 +87,47 @@ public:
         x %= LEN_X;
     }
 
-    void println(const char* str) {
+    void print(const char* str) {
+        if (!str) return;
+
         while(*str) {
             putchar(*str++);
         }
-        y++;
-        x = 0;
+    }
+    
+    void println(const char* str = 0x0) {
+        print(str);
+        new_line();
+    }
+
+    void print(int n) {
+        char str[20];
+        for(int i = 0; i < 20; i++) str[i] = 0;
+        int i = 0;
+        while(n) {
+            str[i] = (n%10) + '0';
+            i++;
+            n /= 10; 
+        }
+        reverse_inplace((unsigned char*) str, i);
+        print(str);
+    }
+
+    // Goes to new line and scrolls
+    void new_line() {
+        if (y < LEN_Y - 1) {
+            y++;
+            x = 0;
+        } else {
+            for (int i = 0; i < LEN_Y - 1; i++) {
+                auto line_index = LEN_X * i;
+                auto line = &mem_buffer[line_index];
+                auto next_line = &mem_buffer[line_index + LEN_X];
+                memcpy((unsigned char*) line, (unsigned char*) next_line, sizeof(*mem_buffer) * LEN_X);
+            }
+            y = LEN_Y - 1;
+            x = 0;
+        }
     }
 
     void set(int x, int y) {
@@ -80,23 +141,13 @@ public:
 
 };
 
-void f(const char* msg){
-    char c; 
-    while(*msg) {
-		asm(
-			"mov %0, %%al;"
-			"mov $0x0E, %%ah;"
-			"int $0x10;"
-			:
-			: "r" (*msg)
-		);
-        msg++;
-	}
-}   
-void kernel_main(void);
+
 extern "C" void __attribute__((noreturn)) boot_main() {
     Terminal t;
     t.clear();
+    t.print(10);
+    t.println();
+    t.println("haha");
     for(int i = 0; i < 80; i++)
         t.putchar(i);
     t.set(0,2);
@@ -104,5 +155,9 @@ extern "C" void __attribute__((noreturn)) boot_main() {
     t.println(msg);
     t.println(msg);
     t.println(msg);
+    for(int i = 0; i < 10000; i++) {
+        t.print(i);
+        t.println();
+    }
     for(;;);
 }
